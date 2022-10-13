ccbca5ae07259e1a0b9ac370c0bbdf063d8a34d7
smartguy1196 <smartguy1196@gmail.com>
2022-10-12 17:31:22 -0700

    test: add PSES initializer
    - add PSES (PowerShell Editor Services) starter script
      - starts the server
      - no stdout??
      - config files do get gen'd

---
+++ initialize.pses.server.ps1
@@ -0,0 +1,17 @@
+$PSES_BUNDLE_PATH = "C:\#\bin\PowerShellEditorServices"
+$SESSION_TEMP_PATH = "C:\#\var\session\powershell-editor-services"
+
+$command = @(
+    "$PSES_BUNDLE_PATH/PowerShellEditorServices/Start-EditorServices.ps1",
+        "-BundledModulesPath $PSES_BUNDLE_PATH",
+        "-LogPath $SESSION_TEMP_PATH/logs.log",
+        "-SessionDetailsPath $SESSION_TEMP_PATH/session.json",
+        "-FeatureFlags @()",
+        "-AdditionalModules @()",
+        "-HostName 'My Client'",
+        "-HostProfileId 'myclient'",
+        "-HostVersion 1.0.0",
+        "-LogLevel Normal"
+)-join " "
+
+pwsh -NoLogo -NoProfile -Command $command
